a067d36e22b7b9ec65cee5a0c4f3ffc1bf668824
ravnkilde1995 <ravnkilde1995@gmail.com>
2024-09-10 23:28:36 +0200

    Made changes to tests

--- tests/pdf_test.py
+++ tests/pdf_test.py
@@ -1,21 +1,23 @@
-def test_generate_pdf(client, tmp_path):
+import os
+
+def test_generate_pdf(client, test_folder):
     form_data = {
         'recipient_name': 'John Doe',
         'req_number': 'REQ123456',
         'date': '2023-08-29',
         'serial_number': '',
-        'item1': 'Iphone 15',
+        'item1': 'Dette er en test',
         'item1_qty': '1'
     }
 
-    client.application.config['OUTPUT_FOLDER'] = str(tmp_path)
+    client.application.config['OUTPUT_FOLDER'] = test_folder
 
     response = client.post('/pdf/generate_pdf', data=form_data)
 
     assert response.status_code == 200
     assert response.headers['Content-Type'] == 'application/pdf'
 
-    output_file = tmp_path / 'generated.pdf'
+    output_file = os.path.join(test_folder, 'generated.pdf')
     
-    assert output_file.exists()
+    assert os.path.exists(output_file), f"File '{output_file}' was not created."
     print(f"File '{output_file}' was successfully created.")
--- tests/upload_test.py
+++ tests/upload_test.py
@@ -2,10 +2,15 @@ import time
 import os
 
 def test_upload_process(client, test_folder):
-    test_pdf_file = os.path.join(test_folder, 'test_pdf.pdf')
+
+    test_pdf_file = os.path.join(test_folder, 'generated_1.pdf')
+    generated_pdf_file = os.path.join(test_folder, 'generated.pdf')
 
     data = {
-        'files[]': (open(test_pdf_file, 'rb'), 'test_pdf.pdf')
+        'files[]': [
+            (open(test_pdf_file, 'rb'), 'generated_1.pdf'),
+            (open(generated_pdf_file, 'rb'), 'generated.pdf')
+        ]
     }
 
     start_time = time.time()
@@ -14,12 +19,8 @@ def test_upload_process(client, test_folder):
 
     elapsed_time = time.time() - start_time
 
-    assert upload_response.status_code == 200
-    json_data = upload_response.get_json()
-
-    assert json_data[0]['status'] == 'success', f"File processing failed: {json_data}"    
+    assert upload_response.status_code == 200  
 
     assert elapsed_time < 10, f"Upload og behandling tog for lang tid: {elapsed_time:.2f} sekunder"
 
     print(f"Processen tog {elapsed_time:.2f} sekunder.")
-    
