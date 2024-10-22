611186e187053ff2c74d1988a93e3b0cc1e7607a
ravnkilde1995 <ravnkilde1995@gmail.com>
2024-10-22 14:20:59 +0200

    did some refactoring on test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,4 @@
 import pytest
-import os
 from main import create_app
 
 @pytest.fixture()
@@ -12,9 +11,25 @@ def app():
 
 @pytest.fixture(scope='function')
 def client(app):
-    """A test client for the app."""
+    # Test client
     return app.test_client()
 
-@pytest.fixture(scope='session')
-def test_folder():
-    return os.path.join(os.path.dirname(__file__), 'test_data')
+@pytest.fixture
+def form_data(): 
+    return {
+        'recipient_name': 'John Doe',
+        'req_number': 'REQ123456',
+        'date': '2023-08-29',
+        'serial_number': 'SN12345',
+        'item1': 'Item 1',
+        'item1_qty': '1',
+        'item2': 'Item 2',
+        'item2_qty': '2',
+    }
+
+@pytest.fixture
+def generated_pdf(client, form_data):
+
+    response = client.post('/pdf/generate_pdf', data=form_data)
+    assert response.status_code == 200
+    return 'generated.pdf'  
--- tests/upload_test.py
+++ tests/upload_test.py
@@ -1,26 +1,22 @@
-import time
 import os
+import time
 
-def test_upload_process(client, test_folder):
+def test_upload_pdf(client, app, generated_pdf):
+   
+    output_folder = app.config['OUTPUT_FOLDER']
+    generated_pdf_file = os.path.join(output_folder, generated_pdf)
 
-    test_pdf_file = os.path.join(test_folder, 'generated_1.pdf')
-    generated_pdf_file = os.path.join(test_folder, 'generated.pdf')
-
-    data = {
-        'files[]': [
-            (open(test_pdf_file, 'rb'), 'generated_1.pdf'),
-            (open(generated_pdf_file, 'rb'), 'generated.pdf')
-        ]
-    }
-
+    assert os.path.exists(generated_pdf_file), "Generated PDF file does not exist."
+    
     start_time = time.time()
 
-    upload_response = client.post('/upload/upload_pdf', data=data, content_type='multipart/form-data')
+    with open(generated_pdf_file, 'rb') as pdf_file:
+        data = {'files[]': (pdf_file, 'generated.pdf')}
+        upload_response = client.post('/upload/upload_pdf', data=data)
 
-    elapsed_time = time.time() - start_time
+    end_time = time.time()
+    actual_time = end_time - start_time
 
-    assert upload_response.status_code == 200  
-
-    assert elapsed_time < 10, f"Upload og behandling tog for lang tid: {elapsed_time:.2f} sekunder"
-
-    print(f"Processen tog {elapsed_time:.2f} sekunder.")
+    assert upload_response.status_code == 200
+    print(f"Upload process took {actual_time:.2f} seconds.")
+    assert actual_time < 5
